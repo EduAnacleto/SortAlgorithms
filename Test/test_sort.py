efaba25d3d3e579570ac3b9bff473cb7ae99dfcc
EduAnacleto <eduardo.statistic@gmail.com>
2022-06-11 00:25:59 -0300

    automated test (pytest) and merge sort adjust

---
+++ Test/test_sort.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(0, '../')
+
+import sort
+import random    
+import pytest
+
+
+class TestaSortInt:
+    @pytest.fixture
+    def o(self):
+        return sort.Sort()
+
+    @pytest.fixture
+    def lista_aleatoria(self):
+        return [random.randrange(100) for x in range(100)]
+
+    @pytest.mark.parametrize("esperado", [(1) for x in range(1000)])
+    def test_mergeSort(self, esperado, o, lista_aleatoria):
+        assert o.ordenada(o.mergeSort(lista_aleatoria)) == esperado
+
+    @pytest.mark.parametrize("esperado", [(1) for x in range(1000)])
+    def test_insertionSort(self, esperado, o, lista_aleatoria):
+        assert o.ordenada(o.insertionSort(lista_aleatoria)) == esperado
+
+    @pytest.mark.parametrize("esperado", [(1) for x in range(1000)])
+    def test_selectionSort(self, esperado, o, lista_aleatoria):
+        assert o.ordenada(o.selectionSort(lista_aleatoria)) == esperado
+
+    @pytest.mark.parametrize("esperado", [(1) for x in range(1000)])
+    def test_bubbleSort(self, esperado, o, lista_aleatoria):
+        assert o.ordenada(o.bubbleSort(lista_aleatoria)) == esperado
+
+
--- sort.py
+++ sort.py
@@ -21,45 +21,43 @@ class Sort:
     # Sort in O(nlog(n))
     def mergeSort(self, lista):
         width = 1
-        num_components = len(lista)
-        while(width < n):
+        n = len(lista)
+        while width < n:
             l = 0
-            while(l < n):
+            while l < n:
                 r = min(l+(2*width-1), n-1)
                 m = min(l+width-1, n-1)
 
-                merge(lista, l, m, r)
-                l == 2*width
+                self.merge(lista, l, m, r)
+                l += 2*width
             width *= 2
         return lista
 
-    def merge(lista, l, m, r):
+    def merge(self, lista, l, m, r):
         n1 = m - l + 1
         n2 = r - m
         L = [0] * n1
         R = [0] * n2
-        for i in range(n1):
+        for i in range(0, n1):
             L[i] = lista[l + i]
-        for i in range(n2):
+        for i in range(0, n2):
             R[i] = lista[m + i + 1]
 
         i, j, k = 0, 0, l
         while i < n1 and j < n2:
             if L[i] <= R[j]:
-                a[k] = L[i]
+                lista[k] = L[i]
                 i += 1
             else:
-                a[k] = R[j]
+                lista[k] = R[j]
                 j += 1
             k += 1
-
         while i < n1:
-            a[k] = L[i]
+            lista[k] = L[i]
             i += 1
             k += 1
-     
         while j < n2:
-            a[k] = R[j]
+            lista[k] = R[j]
             j += 1
             k += 1
 
